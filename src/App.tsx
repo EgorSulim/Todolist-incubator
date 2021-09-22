8d23ea3da544330dd705fa102c1bc01e957b2985
EgorSulim <87086875+EgorSulim@users.noreply.github.com>
2021-09-22 18:05:11 +0300

    added todolistId

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,107 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import TodoList from "./TodoList";
+import {v1} from "uuid";
 
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+export type FilterValuesType = "all" | "active" | "completed"
+
+type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+type TaskStateType = {
+    [key: string]: Array<TaskType>
+}
 
 function App() {
-    let initTask1 = [
-        {id: 1, title: 'HTML&CSS1', isDone: true},
-        {id: 2, title: 'JS1', isDone: true},
-        {id: 3, title: 'React1', isDone: false},
-        {id: 4, title: 'Redux', isDone: false},
-    ]
 
-    let arr = useState(initTask1)
-    let task1 = arr[0]
-    let setTask1 = arr[1]
+    const todoListID_1 = v1()
+    const todoListID_2 = v1()
 
-    function removeTask(id: number) {
-        let filteredTask1 = task1.filter(t => t.id !== id)
-        setTask1(filteredTask1)
+    const [todoLists, setTodoLists] = useState<Array<TodolistType>>([
+        {id: todoListID_1, title: 'What to learn', filter: 'all'},
+        {id: todoListID_2, title: 'What to buy', filter: 'all'}
+    ])
+
+    const [tasks, setTasks] = useState<TaskStateType>({
+        [todoListID_1]: [
+            {id: v1(), title: "JS", isDone: false},
+            {id: v1(), title: "CSS", isDone: false},
+            {id: v1(), title: "HTML", isDone: true},
+
+        ],
+        [todoListID_2]: [
+            {id: v1(), title: "Milk", isDone: false},
+            {id: v1(), title: "Bread", isDone: false},
+            {id: v1(), title: "Meat", isDone: true},
+
+        ]
+    })
+
+    const removeTask = (taskID: string, todolistID: string) => {
+        tasks[todolistID] = tasks[todolistID].filter(t => t.id !== taskID)
+        setTasks({...tasks})
     }
+    const addTask = (title: string, todolistID: string) => {
+        const newTask: TaskType = {
+            id: v1(),
+            title: title,
+            isDone: false
+        }
+        tasks[todolistID] = [newTask, ...tasks[todolistID]]
+        setTasks({...tasks})
+    }
+    const changeTaskStatus = (taskID: string, isDone: boolean, todolistID:string) => {
+        tasks[todolistID] = tasks[todolistID].map(t => t.id === taskID ? {...t, isDone: isDone} : t)
+        setTasks({...tasks})
+    }
+    const changeTodoListFilter = (filter: FilterValuesType, todolistID: string) => {
+        setTodoLists(todoLists.map(tl => tl.id === todolistID ? {...tl, filter: filter} : tl))
+    }
+    const removeTodoList=(todoListID: string)=>{
+        setTodoLists(todoLists.filter(tl=>tl.id !== todoListID))
+        delete tasks[todoListID]
+    }
+    //UI:
+    const TodoListsComponents=todoLists.map(tl=>{
+        let tasksForRender = tasks[tl.id]
+        if (tl.filter === "active") {
+            tasksForRender = tasks[tl.id].filter(t => t.isDone === false)
+        }
+        if (tl.filter === "completed") {
+            tasksForRender = tasks[tl.id].filter(t => t.isDone === true)
+        }
+        return(
+            <div className="App">
+                <TodoList // TodoList()
+                    removeTodoList={removeTodoList}
+                    key={tl.id}
+                    id={tl.id}
+                    filter={tl.filter}
+                    title={tl.title}
+                    tasks={tasksForRender}
+                    addTask={addTask}
+                    removeTask={removeTask}
+                    changeTaskStatus={changeTaskStatus}
+                    changeTodoListFilter={changeTodoListFilter}
+                />
+            </div>
+        )
+    })
+    //
 
 
     return (
         <div className="App">
-            <Todolist
-                removeTask={removeTask}
-                title={'Todolist1'}
-                task={task1}/>
-
+            {TodoListsComponents}
         </div>
     );
 }
